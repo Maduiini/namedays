87b17c9341324f091562d20251cd5dd03405b529
Maduiini <matias.laitinen@gmail.com>
2018-01-10 19:48:26 +0200

    Completed the demo task exercise
    Added some validation and reworked the demo task to use DateTime instead
    of string, adding some validation and exception handling. Added
    possibility to read multiple files without a specific filename.
    Commented code.

--- NameDays/NameDays/NameDays.cs
+++ NameDays/NameDays/NameDays.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -13,23 +14,61 @@ namespace NameDays
     /// </summary>
     class NameDays
     {
-        private static char tupleSeparator = ';';
-        private static string filePath = @"\..\..\data\nimet.csv";
+        private static char columnSeparator = ';';
+        private static char dateSeparator = '.';
+        private static string dataPathExt = @"\..\..\data\"; // Used with the present running directory
+
+        /// <summary>
+        /// Gets all names that a desired nameday has, parsing a date from a string.
+        /// We are not interested in years as they are irrelevant.
+        /// </summary>
+        /// <param name="dateStr">Desired nameday date string</param>
+        /// <returns>Namedays separated by comma</returns>
+        public static string ListNameDays(string dateStr)
+        {
+            DateTime date = DateTime.MinValue;
+            string[] dateParts = dateStr.Split(dateSeparator);
+            if ((dateParts[0] != null) && (dateParts[1] != null))
+            {
+                // Parse the date from string
+                try
+                {
+                    date = date.AddDays(double.Parse(dateParts[0]) - 1);
+                    date = date.AddMonths(int.Parse(dateParts[1]) - 1);
+                }
+                // Date is in an incorrect format ie. 24.13., unable to parse it.
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("Incorrect date format. \n--\n" + ex.Message);
+                }
+            }
+            else
+            {
+                // Date is in an incorrect format, unable to parse it.
+                Console.WriteLine("Please specify a date as a parameter in format 'd.m.' ");
+                return "";
+            }
+
+            return ListNameDays(date);
+        }
 
         /// <summary>
         /// Gets all names that a desired nameday has, reading an external file to get the data.
         /// </summary>
         /// <param name="date">Desired nameday date</param>
-        /// <returns>List of names on the desired nameday</returns>
-        public static string GetNamesForDate(string date)
+        /// <returns>Namedays separated by ', '</returns>
+        public static string ListNameDays(DateTime date)
         {
-            string currentPath = CurrentRunningPath();
-            string path = currentPath + filePath;
+            string currentPath = PresentRunningDirectory();
+            string path = currentPath + dataPathExt;
             List<string[]> nameDayNames = ReadNameDayPairs(path);
 
             for (int i = 0; i < nameDayNames.Count; i++)
             {
-                if (nameDayNames[i][0].Equals(date))
+                double targetDay = Double.Parse(nameDayNames[i][0].Split(dateSeparator)[0]);
+                int targetMonth = Int32.Parse(nameDayNames[i][0].Split(dateSeparator)[1]);
+
+                if (targetMonth.Equals(date.Month) && targetDay.Equals(date.Day))
                 {
                     return nameDayNames[i][1];
                 }
@@ -38,41 +77,45 @@ namespace NameDays
         }
 
         /// <summary>
-        /// Finds and returns the current running path of the program.
+        /// Finds and returns the current running directory of the program.
         /// </summary>
         /// <returns>The path of the program</returns>
-        public static string CurrentRunningPath()
+        private static string PresentRunningDirectory()
         {
             return Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
         }
 
         /// <summary>
-        /// Reads a csv file where cells are separated as ; and rows on a new line, returns its contents as a list of tuples.
+        /// Reads all .csv files in the data folder where cells are separated with ';' and rows on a new line.
+        /// Returns contents as a string[2] list.
         /// </summary>
-        /// <param name="path">File path</param>
+        /// <param name="path">Data folder path</param>
         /// <returns>List of pairs in the file, separated by ;</returns>
-        public static List<string[]> ReadNameDayPairs(string path)
+        private static List<string[]> ReadNameDayPairs(string path)
         {
             List<string[]> nameDayPairs = new List<string[]>();
             StreamReader file;
             string[] splitLine = new string[2];
             string line;
+            string[] files = Directory.GetFiles(@path, "*.csv");
 
-            if (File.Exists(@path))
+            // Read all .csv files in data folder with StreamReader
+            for (int i = 0; i < files.Length; i++)
             {
-                file = new StreamReader(@path);
-                int i = 0;
-                while ((line = file.ReadLine()) != null)
+                if (File.Exists(@files[i]))
                 {
-                    splitLine = line.Split(tupleSeparator);
-                    nameDayPairs.Add(splitLine);
-                    i++;
+                    file = new StreamReader(files[i]);
+                    int j = 0;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        splitLine = line.Split(columnSeparator);
+                        nameDayPairs.Add(splitLine);
+                        j++;
+                    }
+                    file.Close();
                 }
-                file.Close();
             }
-
             return nameDayPairs;
         }
     }
 }
-
--- NameDays/NameDays/Program.cs
+++ NameDays/NameDays/Program.cs
@@ -9,17 +9,19 @@ namespace NameDays
 
     /// <summary>
     /// Console Program which returns name days for a selected date.
+    /// Data for corresponding namedays is stored in the /data folder.
+    /// Multiple .csv files can be used, containing the namedays and
+    /// any filename will do as long as it's a .csv file and in a right format.
     /// </summary>
     class Program
     {
-
         /// <summary>
-        /// The main program to be run in the console.
+        /// The main console program
         /// </summary>
         /// <param name="args">parameters passed when run, to indicate which date is selected.</param>
         public static void Main(string[] args)
         {
-
+            // user specified no args, cannot fetch namedays.
             if (args.Length <= 0)
             {
                 Console.WriteLine("Please specify a date as a parameter in format 'd.m.' ");
@@ -27,23 +29,22 @@ namespace NameDays
             }
             string date = args[0];
 
-            // Try to get the names for the selected date.
+            // Try to get the namedays for the selected date.
             try
             {
-                string names = NameDays.GetNamesForDate(date.ToString());
-                Console.Write("Names on " + date + " : " + names); // TODO REMOVE
+                string names = NameDays.ListNameDays(date);
+                Console.Write("Namedays: " + names);
             }
-            catch (NotImplementedException ex)
-            {
-                Console.WriteLine("Found a method that is yet unimplemented. Please finish what you've started. :) \n--\n" + ex.Message);
-            }
+            // Failure to read .csv file data
             catch (DirectoryNotFoundException ex)
             {
                 Console.WriteLine("Incorrect File path. \n--\n" + ex.Message);
             }
-            catch (ArgumentOutOfRangeException ex)
+            // Any other failure is caught here
+            // TODO: Specify more exceptions
+            catch (Exception ex)
             {
-                Console.WriteLine("Argument out of range. \n--\n" + ex.Message);
+                Console.WriteLine("There was an error while trying to get the namedays. \n--\n" + ex.Message);
             }
         }
     }
